eb407dbeb4220f9ee23ea15cd0508ac3a8eb6795
Adrian Singer <adrian.singer@iwu.fraunhofer.de>
2021-01-25 22:49:13 +0100

    Secret constructors updated

--- src/TeleScope.Connectors.Abstractions/Secrets/Secret.cs
+++ src/TeleScope.Connectors.Abstractions/Secrets/Secret.cs
@@ -7,9 +7,26 @@ namespace TeleScope.Connectors.Abstractions.Secrets
 {
 	public class Secret : ISecret
 	{
+		// -- properties
+
 		public string Name { get; set; }
 
 		public SecureString Password { get; private set; }
+
+		// -- constructors
+
+		public Secret()
+		{
+
+		}
+
+		public Secret(string name, string password)
+		{
+			Name = name;
+			SetPassword(password);
+		}
+
+		// -- methods
 
 		public void SetPassword(in string password)
 		{

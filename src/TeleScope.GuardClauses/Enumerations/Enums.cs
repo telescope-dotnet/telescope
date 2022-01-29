5974fef5bf8d8f5a3b73092c267511ec63792be2
Adrian Singer <Adrian.Singer@iwu.fraunhofer.de>
2022-01-29 22:47:38 +0100

    guard functions for string validations improved

---
+++ src/TeleScope.GuardClauses/Enumerations/Enums.cs
@@ -0,0 +1,21 @@
+﻿using TeleScope.GuardClauses.Abstractions;
+
+namespace TeleScope.GuardClauses.Enumerations
+{
+	/// <summary>
+	/// This enumeration is used to specify the IP version for guarding  IP strings.
+	/// It is used in classes, based on the <see cref="IStringGuardable"/> interface.
+	/// The .NET build-in enumerations are not used in order to prevent false parameter settings.
+	/// </summary>
+	public enum InternetProtocols
+	{
+		/// <summary>
+		/// Internet Protocol version 4.
+		/// </summary>
+		IPv4,
+		/// <summary>
+		/// Internet Protocol version 6.
+		/// </summary>
+		IPv6
+	}
+}
